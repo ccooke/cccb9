de1b82d0fd14153d8c7e226d4fab38d9cde72131
C. Cooke <ccooke-git@gkhs.net>
2014-03-11 16:30:17 +0000

    First part of a generic 'execute anything' module

--- lib/cccb/core/pom.rb
+++ lib/cccb/core/pom.rb
@@ -6,5 +6,15 @@ module CCCB::Core::Pom
     add_request :pom, /^pom$/i do |m, s|
       %x{pom}
     end
+
+    add_request :ebook, /^add fanfic (?<url>.*)/i do |match,message|
+      if message.user.get_setting( "privs", "allow_fanfic" )
+        %x{fanfic-fetch #{match[:url]}}.each_line do |line|
+          message.reply line
+        end
+      else
+        "You need the allow_fanfic privilege"
+      end
+    end
   end
 end
