b2ac7a1f46b6cf787a6cc2be9a8bcb59ee6e96aa
C. Cooke <ccooke-git@gkhs.net>
2014-03-11 16:32:13 +0000

    New module: Usertracking, with 'seen' and 'tell' requests

---
+++ lib/cccb/core/usertrack.rb
@@ -0,0 +1,99 @@
+require 'tzinfo'
+require 'tzinfo/data'
+
+module CCCB::Core::UserTrack
+  extend Module::Requirements
+
+  needs :bot, :session
+
+  def usertime(user, time = nil)
+    use_time ||= Time.now
+    if timezone_name = user.get_setting("options", "timezone")
+      timezone = ::TZInfo::Timezone.get(timezone_name)
+      offset = timezone.current_period.offset.utc_offset
+      hours = offset / 3600
+      minutes = offset % 3600
+      usable_offset = format("%+03d:%02d", hours, minutes)
+      puts "OFFSET: #{usable_offset}"
+
+      use_time.localtime(usable_offset)
+    else
+      use_time
+    end
+  end
+
+  def module_load
+
+    add_setting :user, "user_tracking", auth: :superuser, secret: true
+
+    add_hook :user_tracking, :pre_setting_set do |obj, setting, hash|
+      next unless obj.is_a? CCCB::User
+      next unless setting == "options" and hash.include? "timezone"
+      begin
+        ::TZInfo::Timezone.get(hash['timezone'])
+      rescue 
+        raise "Invalid timezone: #{hash['timezone']}. Pick the closest entry in http://en.wikipedia.org/wiki/List_of_tz_database_time_zones e.g.: Europe/London"
+      end
+    end
+
+    add_hook :user_tracking, [:join, :nick, :privmsg] do |message|
+      if tells = message.user.get_setting("user_tracking", "tells")
+        tells.delete_if do |(user,time,tell)|
+          message.network.msg message.user.name, "#{user} left a message at #{usertime(message.user,time)}: #{tell}"
+          true
+        end
+      end
+    end
+
+    add_request :user_tracking, /^\s*seen\s+(?<user>\S+)\s*$/i do |match, message|
+      user = message.network.get_user(match[:user].downcase)
+      if user == message.user
+        "I don't know. Have you seen yourself?"
+      elsif user.name == message.network.nick
+        "Yes."
+      else
+        "I last saw #{user} active at #{usertime( message.user, user.history.last.time )}#{
+          if message.to_channel? and user.channel_history.include? message.channel.name
+            saved_message = user.channel_history[message.channel.name]
+            text = if saved_message.ctcp?
+              saved_message.format("* %(nick_with_mode) %(ctcp_text")
+            else
+              saved_message.format("<%(nick_with_mode)> %(text)")
+            end
+            " saying '#{text}'"
+          end
+        }. #{
+          if user.channels.count == 0
+            "I can't see them online at the moment, though."
+          elsif user.channels.any? { |c| message.user.channels.include? c }
+            "I can see them currently online in at least one channel you're in."
+          else
+            "You can't see them online at the moment, though."
+          end
+        }"
+      end
+    end
+
+    add_request :user_tracking, /^\s*tell\s+(?<user>\S+)\s+(?<note>.*?)\s*$/i do |match, message|
+      user = message.network.get_user(match[:user].downcase)
+      tells = user.get_setting("user_tracking", "tells")
+      if tells.nil?
+        tells = []
+        user.set_setting( tells, "user_tracking", "tells")
+      end      
+
+      if user.channels.count == 0
+        tells << [ message.user, message.time, match[:note] ]
+        "Ok"
+      else
+        "They appear to still be online. Try sending that to them directly."
+      end
+    end
+
+#    add_request :user_tracking, /^\s*channels\s+(?<user>\S+)\s*$/ do |match, message|
+#      user = message.network.get_user(match[:user].downcase)
+#      user.channels
+#    end
+
+  end
+end
