9888fef4e32bc01a416f9c9cdf016fd9200efbed
C. Cooke <ccooke-git@gkhs.net>
2014-09-01 01:12:46 +0100

    refactor, simplify, improve

--- lib/cccb/core/uri_detection.rb
+++ lib/cccb/core/uri_detection.rb
@@ -5,43 +5,14 @@ module CCCB::Core::URIDetection
   needs :bot
 
   URL_REGEX = /
-    (?<uri>
-      (?<protocol>
-        \p{Word}+
-        :
-        (?:
-          \/\/
-        )?
-      )?
-      (?<server>
-        (?:
-          \p{Word}+
-          @
-        )?
-        \p{Word}+
-        (?<domain>
-          \.
-          \p{Word}+
-        )*
-        (?:
-          : 
-          \d+
-        )?
-      )
-      (?<path>
-        (?:
-          \/
-          [\p{Word}%]+
-        )*
-        (?:
-          (?:
-            \?
-          |
-            #
-          )
-          \S+
-        )*
-      )?
+    (?<protocol> \p{Word}+ : (?: \/\/)?){0}
+    (?<server> (?: \p{Word}+ @)?  \p{Word}+ (?<domain> \.  \p{Word}+)* (?: : \d+)?){0}
+    (?<path> \/ [\p{Word}.%]+ (?: (?: \?  | \#) \S+)* ){0}
+
+    (?<uri> 
+      \g<protocol> \g<server> \g<path>*
+    | 
+      \g<server> \g<path>+
     )
   /x
 
@@ -54,12 +25,14 @@ module CCCB::Core::URIDetection
     end
 
     add_hook :uri_detection, :message do |message|
-      debug("In uri detection")
+      info("In uri detection")
       text = message.text
+      info "message: #{message}"
       next unless message.user.get_setting( "options", "uri_events" )
+      info "enabled"
       while match = URL_REGEX.match( text )
+        info "URI: #{match}"
         text = match.post_match
-        next if match[:server].nil? or match[:domain].nil? || ( match[:protocol].nil? && match[:path] == "" )
         schedule_hook :uri_found, message, {
           uri: match[:uri],
           protocol: match[:protocol] || "http://",
