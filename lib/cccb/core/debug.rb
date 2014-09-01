7d186368087cf64d46ea73b0d7e50fb73f057f42
C. Cooke <ccooke-git@gkhs.net>
2014-09-01 01:16:48 +0100

    refactor namespace, move loglevel to the right place

--- lib/cccb/core/debug.rb
+++ lib/cccb/core/debug.rb
@@ -1,10 +1,10 @@
 module CCCB::Core::Debugging
   extend Module::Requirements
 
-  needs :bot, :commands
+  needs :bot, :commands, :logging
 
   def module_load
-    add_command :debug, "set hook trace" do |message, hooks|
+    add_command :debug, "admin trace add" do |message, hooks|
       default_setting true, "allowed_features", "debug_hook_trace"
       target = if message.to_channel? 
         message.channel
@@ -14,14 +14,14 @@ module CCCB::Core::Debugging
       result = []
       hooks.map(&:to_sym).each do |h|
         message.reply("Adding debug hook to #{h}")
-        add_hook :debug_hook_trace, h do |*args|
+        add_hook :debug_hook_trace, h, top: true do |*args|
           target.msg "<debug: hook=#{h} args=#{args.inspect}>"
         end
         result << "Trace hooks for #{h}: #{get_hooks( :debug_hook_trace, h )}"
       end
     end
 
-    add_command :debug, "unset hook trace" do |message, hooks|
+    add_command :debug, "admin trace remove" do |message, hooks|
       hooks.map(&:to_sym).each do |h|
         trace_hooks = get_hooks( :debug_hook_trace, h )
         trace_hooks.each do |tr_h|
@@ -37,5 +37,21 @@ module CCCB::Core::Debugging
         message.network.puts "PART #{channel}"
       end
     end
+
+    add_command :debug, "admin loglevel" do |message, (level)|
+      message.reply( if level.nil?
+        "System loglevel is currently #{logging.number_to_const[logging.loglevel]}"
+      else
+        auth_command :superuser, message
+        level = level.to_s.upcase.to_sym
+        if self.class.constants.include? level
+          logging.loglevel = self.class.const_get( level )
+          "Okay, logging is now set to #{level}"
+        else
+          "No such log level #{level}"
+        end
+      end )
+    end
+
   end
 end
