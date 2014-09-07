1efd1e01578b8b1fbf5cce13f6fa9a087d3bf5b2
C. Cooke <ccooke-git@gkhs.net>
2014-09-07 22:20:23 +0100

    Provide the ability to run commands in a forked process

---
+++ lib/module/requirements/feature/background.rb
@@ -0,0 +1,58 @@
+
+class Backgrounder
+  def initialize(obj)
+    @obj = obj
+  end
+
+  def method_missing(sym, *args, **kwargs)
+    args += [ **kwargs ] unless kwargs.empty?
+    debug "Sending #{sym} args: #{args.inspect}"
+    @obj.send(sym,*args)
+  end
+
+  def background(sym, *args, **kwargs)
+    (r, w) = IO.pipe
+    pid = fork do
+      begin
+        r.close
+        CCCB.instance.set_log_tag pid: $$.to_s
+        CCCB.instance.logging_transition_unthreaded
+        args += [ **kwargs ] unless kwargs.empty?
+        verbose "Sending #{sym} args: #{args}"
+        data = @obj.send(sym,*args)
+        verbose "Got #{data}"
+        dump = Marshal.dump(data)
+        w.write dump
+      rescue Exception => e
+        critical "Exception: #{e} #{e.backtrace}"
+        w.write Marshal.dump(e)
+      ensure
+        w.close
+      end
+
+    end
+
+    w.close
+    verbose "In parent, #{r}"
+    data = r.read
+    r.close
+    verbose "Waiting for #{pid}"
+    Process.kill "TERM",  pid
+    verbose "Cleaned up"
+    out = Marshal.load( data )
+
+    verbose "Here: #{out}"
+
+    out
+  end
+end
+
+
+module Module::Requirements::Feature::Background
+  extend Module::Requirements
+  needs :logging
+
+  def background
+    Backgrounder.new(self)
+  end
+end
