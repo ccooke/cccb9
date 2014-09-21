0c947f1235f02e3c7a13582062f78c9102f5cbea
C. Cooke <ccooke-git@gkhs.net>
2014-09-22 00:04:28 +0100

    Use reply objects properly in commands

--- lib/cccb/core/core_commands.rb
+++ lib/cccb/core/core_commands.rb
@@ -7,13 +7,13 @@ module CCCB::Core::CoreCommands
 
     add_command :debug, "show load errors" do |message, args|
       auth_command :superuser, message
-      message.reply "Last (re)load at #{$load_time}"
-      message.reply $load_errors
+      message.reply.force_title = "Last (re)load at #{$load_time}"
+      message.reply.summary = $load_errors.dup + [ "-- #{$load_errors.count} errors" ]
     end
     
     add_command :core, [ ["copy","cp"] ] do |message, args|
       raise "Two arguments are required" unless args.count == 2
-      message.reply copy_user_setting( message, args[0], args[1] )
+      message.reply.summary = copy_user_setting( message, args[0], args[1] )
     end
 
     add_command :core, [ ["set","setting"], ["my","channel",""] ] do |message, args, words|
@@ -30,25 +30,28 @@ module CCCB::Core::CoreCommands
           "user"
         end
         setting = parse_setting( setting, message, default_type )
-        message.reply user_setting( message, setting, value )
+        message.reply.summary = user_setting( message, setting, value )
       end
     end
 
     add_command :core, "admin reload" do |message|
       auth_command :superuser, message
+      message.clear_reply
       reload_then(message) do |m|
         if $load_errors.count == 0
-          m.reply "Ok"
+          m.reply.title = "Reload successful"
         else
-          m.reply "There were #{$load_errors.count} error(s):"
-          $load_errors.each { |e| m.reply e }
+          m.reply.force_title = "Reload failed: #{$load_errors.count} error(s):"
+          m.reply.summary = $load_errors.dup
         end
+        # This needs to be here because this code executes after the reload
+        m.send_reply
       end
     end
 
     add_command :core, "admin superuser enable" do |message, args|
       password_valid = (args.join(" ") == CCCB.instance.superuser_password)
-      message.reply( if message.to_channel?
+      message.reply.summary = if message.to_channel?
         if password_valid
           CCCB.instance.superuser_password = (1..32).map { (rand(64) + 32).chr }.join
         end
@@ -60,24 +63,24 @@ module CCCB::Core::CoreCommands
         else
           "Denied"
         end
-      end)
+      end
     end
 
     add_command :core, "admin superuser disable" do |message|
-      message.reply( if message.user.superuser?
+      message.reply.summary = if message.user.superuser?
         get_setting("superusers").delete message.from.downcase.to_s
         "Removed you from the superuser list."
       else
         "You weren't a superuser in the first place."
-      end)
+      end
     end
 
     add_command :core, [ "admin superuser", [ "status", "" ] ] do |message|
-      message.reply( if message.user.superuser?
+      message.reply.summary = if message.user.superuser?
         "You are a superuser"
       else
         "You are not a superuser"
-      end )
+      end
     end
 
     add_command :core, "admin reconnect" do |message,args|
