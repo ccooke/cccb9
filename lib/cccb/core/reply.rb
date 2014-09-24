09436ca6575eea1ed53dbec98a789eaa79e3c1d1
C. Cooke <ccooke-git@gkhs.net>
2014-09-24 02:00:13 +0100

    Add Redcarpet parsers to reply code

--- lib/cccb/core/reply.rb
+++ lib/cccb/core/reply.rb
@@ -1,5 +1,62 @@
+require 'redcarpet'
 
 class CCCB::Reply
+  class IRCRender < Redcarpet::Render::Base
+    def normal_text(text)
+      text
+    end
+    def block_code(code,language = nil)
+      block = if language 
+        [ "|== #{bold code} ==", ] 
+      else
+        []
+      end
+      block += normal_text(code).each_line.map do |l|
+        l.strip.gsub /^/, '| '
+      end
+      block.join("\n")
+    end
+    def codespan(code)
+      block_code(code,nil)
+    end
+    def bold(text)
+      "\x02#{text}\x02"
+    end
+    def header(title,level)
+      ("[" * level) + bold(title) + ("]" * level) + "\n"
+    end
+    def double_emphasis(text)
+      bold text
+    end
+    def emphasis(text)
+      bold text
+    end
+    def linebreak(text)
+      "\n"
+    end
+    def paragraph(text)
+      text + "\n"
+    end
+    def list(content,list_type)
+      index = 0
+      content.split("\x03").map do |i|
+        case list_type
+        when :ordered
+          index += 1
+          "#{index}. #{i}"
+        when :unordered
+          "* #{i}"
+        end
+      end.join("\n") + "\n"
+    end
+    def list_item(content, list_type)
+      "#{content}\x03"
+    end
+    def link(link,title,content)
+      content
+    end
+  end
+
   @categories = %i{ title summary fulltext }
 
   def initialize(message)
@@ -36,26 +93,37 @@ class CCCB::Reply
 
   def minimal_form
     return self.short_form if @no_minimal
-    { text: [ self.summary || self.title || self.fulltext ].flatten }
+    [ self.summary || self.title || self.fulltext ].flatten.join("\n")
   end
 
   def short_form
-    { 
-      title: [ *self.title ].first, 
-      text: [ self.summary || self.fulltext ]
-    }
+    "# #{[ *self.title ].first}\n#{self.summary || self.fulltext}"
   end
 
   def long_form
-    {
-      title: [ *self.title ].first,
-      text: [ self.fulltext || self.summary ]
-    }
+    "# #{[ *self.title ].first}\n#{self.fulltext || self.summary}"
   end
     
 end
 
 module CCCB::Core::Reply
   extend Module::Requirements
+
+  def module_load
+    reply.irc_parser = Redcarpet::Markdown.new( 
+      CCCB::Reply::IRCRender,
+      no_intra_emphasis: true,
+      lax_spacing: true,
+      quote: false,
+      footnotes: false,
+      tables: true
+    )
+    reply.web_parser = Redcarpet::Markdown.new( 
+      Redcarpet::Render::HTML, 
+      autolink: true,
+      footnotes: true,
+      tables: true
+    )
+  end
 
 end
