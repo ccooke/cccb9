51d676da6c61b674d2c44772d855b266a500958d
C. Cooke <ccooke-git@gkhs.net>
2015-06-04 02:05:49 +0100

    Import event code from cccb8

--- lib/module/requirements/feature/events.rb
+++ lib/module/requirements/feature/events.rb
@@ -8,29 +8,32 @@ module Module::Requirements::Feature::Events
       :hook,
       :name
     ]
-    def initialize(hash = {})
+    def initialize(**hash)
       raise InvalidEvent.new(hash) unless REQUIRED.all? { |k| hash.include? k }
-      super(hash)
+      super(**hash)
+    end
+
+    def persist?
+      info "Does #{self} persist? No"
+      false
     end
   end
 
-  needs :hooks, :managed_threading
+  needs :hooks
 
   def add_timer(frequency, &block)
     auto_timer_hook = :"auto_timer_#{block.object_id}"
 
-    events.lock.synchronize do
-      add_hook :events, auto_timer_hook, block
-      add_event frequency: frequency, hook: auto_timer_hook, name: auto_timer_hook, start_time: Time.now + frequency
-    end
+    add_hook :events, auto_timer_hook, &block
+    add_event frequency: frequency, hook: auto_timer_hook, name: auto_timer_hook, start_time: Time.now + frequency
 
     auto_timer_hook
   end
 
-  def add_event(*args)
+  def add_event(**args)
     events.lock.synchronize do
-      events.db << Event.new(args)
-      debug "Added event: #{events.db.last}"
+      events.db << Event.new(**args)
+      spam "Added event: #{events.db.last}"
       events.db = sort_events
     end
   end
@@ -41,26 +44,41 @@ module Module::Requirements::Feature::Events
     end
   end
 
-  def module_start
+  def module_load
     events.lock ||= Mutex.new
-    events.db ||= []
-    ManagedThread.new :events, start: true, repeat: 1, restart: true do
-      events.lock.synchronize do
-        time = Time.now
-        spam "Events since #{time}: #{events.db}"
-        while events.db.count > 0 and events.db.first.start_time <= time
-          event = events.db.shift
-          debug "Got event #{info}"
-          schedule_hook event.hook, event
-          if event.recurrs
-            event.start_time += event.frequency while event.start_time <= time
-            events.db << event
+    events.db = []
+  end
+
+  def module_start
+    events.thread = Thread.new do
+
+      loop do
+        sleep(1)
+        spam "In timer thread"
+        events.lock.synchronize do
+          time = Time.now
+          spam "Events since #{time}: #{events.db}"
+
+          while events.db.count > 0 and events.db.first.start_time <= time
+            event = events.db.shift
+            debug "Got event #{info}"
+            schedule_hook event.hook, event
+            if event.recurrs
+              event.start_time += event.frequency while event.start_time <= time
+              events.db << event
+            end
+            
+            events.db = sort_events
           end
-          
-          events.db = sort_events
+
         end
       end
+
     end
+  end
+
+  def module_unload
+    events.thread.kill if events.thread
   end
 
 end
